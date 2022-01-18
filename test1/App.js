3b2781dd80a2ab9989249e2a13e72548a1c071de
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-01-18 20:46:49 -0300

    1st commit

---
+++ test1/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text> Hello AppShop </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
