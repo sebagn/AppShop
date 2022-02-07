8af4d1f4a40171012d4452e436c10e1eec4e02a1
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-02-07 20:53:33 -0300

    empezando auth

--- test1/src/navigation/index.js
+++ test1/src/navigation/index.js
@@ -1,39 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import ShopStackNavigation from './shop';
-import CartStackNavigation from './cart';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
-const BottomTabs = createBottomTabNavigator();
+import BottomTabsNavigator from './bottom-tabs';
+import AuthNavigator from './auth';
 
 const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <BottomTabs.Navigator
-        initialRouteName="Shop"
-        screenOptions={{
-          headerShown: false,
-        }}>
-            <BottomTabs.Screen
-            name="Shop"
-            component={ShopStackNavigation}
-            options={{
-                tabBarIcon: ({focused}) => (
-                <Ionicons name="home-outline" size={20} />
-                ),
-            }}
-            />
-            <BottomTabs.Screen
-            name="Cart"
-            component={CartStackNavigation}
-            options={{
-                tabBarIcon: ({focused}) => (
-                <Ionicons name="cart-outline" size={20} />
-                ),
-            }}
-            />
-      </BottomTabs.Navigator>
+      <AuthNavigator />
+      {/* <BottomTabsNavigator /> */}
     </NavigationContainer>
   );
 };
